d43cdbd8e4ed432a60e970d4bb100457fa4ede86
DavidNeiOs <davidneios25@gmail.com>
2019-05-19 06:57:24 -0400

    add and remove task functionality added

---
+++ app.js
@@ -0,0 +1,59 @@
+// UI vars
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners function
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener("submit", addTask);
+  // Remove task event
+  taskList.addEventListener("click", removeTask);
+}
+
+// Add task function
+function addTask(e) {
+  e.preventDefault();
+
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  // Create li element
+  const li = document.createElement("li");
+  li.className = "collection-item";
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  // Create new link element
+  const link = document.createElement("a");
+  link.className = "delete-item secondary-content";
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append link to li
+  li.appendChild(link);
+
+  // Append the li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = "";
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (
+      confirm(
+        `Are you sure you want to delete task "${
+          e.target.parentElement.parentElement.textContent
+        }" ?`
+      )
+    )
+      e.target.parentElement.parentElement.remove();
+  }
+}
